16e6b5ac11bb998f98f8f4d368bedf99f9123217
Felipao98 <80127519+Felipao98@users.noreply.github.com>
2025-05-15 16:16:31 -0300

    Add files via upload

--- Gestao_Agendamento/urls.py
+++ Gestao_Agendamento/urls.py
@@ -4,7 +4,8 @@ from . import views
 from django.shortcuts import render
 # teste local
 urlpatterns = [
-    path('', views.render_template, name='home'),
+    # path('', views.render_template, name='home'),
+    path('teste/', views.teste_controller, name='teste_controller'),
     path('inserir-agendamento/', views.inserir_agendamento, name='inserir_agendamento'),
     path('remover-agendamento/', views.remover_agendamento, name='remover_agendamento'),
     path('atualizar-agendamento/', views.atualiza_agendamento, name='atualizar_agendamento'),
