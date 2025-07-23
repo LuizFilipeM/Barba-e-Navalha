16ad0e248d537cefbe6a66b3064cf23b6fd621a1
V1torL <102988485+V1torL@users.noreply.github.com>
2025-07-22 21:03:50 -0300

    .

--- Projeto/settings.py
+++ Projeto/settings.py
@@ -127,7 +127,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'barbaNavalha',
         'USER': 'postgres',
-        'PASSWORD': 'Vlrc2002',
+        'PASSWORD': '-----',
         'HOST': 'localhost',
         'PORT': '5433',
     }
